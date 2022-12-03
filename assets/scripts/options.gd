41ed60823f944f75193a534431288b45d5c1394d
Damien Kusters <damien.kusters@ziggo.nl>
2022-12-03 14:35:12 +0100

    Audio and change Ascention music

--- assets/scripts/options.gd
+++ assets/scripts/options.gd
@@ -1,7 +1,28 @@
 extends Node
 
+func _ready():
+	_on_MusicSlider_value_changed(1);
+	_on_SoundSlider_value_changed(3);
+	
 func exportSave():
 	$HBoxContainer/SaveTextEdit.text = Globals.exportSave();
 
 func importSave():
 	Globals.importSave($HBoxContainer/SaveTextEdit.text.strip_edges(true,true));
+
+func _on_MusicSlider_value_changed(value):
+	if value == 0:
+		AudioServer.set_bus_mute(1, true);
+	else:
+		AudioServer.set_bus_mute(1, false);
+		AudioServer.set_bus_volume_db(1, value)
+	pass # Replace with function body.
+
+
+func _on_SoundSlider_value_changed(value):
+	if value == 0:
+		AudioServer.set_bus_mute(2, true);
+	else:
+		AudioServer.set_bus_mute(2, false);
+		AudioServer.set_bus_volume_db(2, value)
+	pass # Replace with function body.
